1900e088aa5bf62a2769a09c23519e17f97ecd36
Nazmi <nazmifeeroz@hotmail.com>
2017-11-27 08:00:07 +0800

    api for update and delete done

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,8 +4,43 @@ module Api::V1
     
     def index
       
-      @ideas = Idea.all
+      @ideas = Idea.order("created_at DESC")
       render json: @ideas
+      
+    end
+
+    def create
+
+      @idea = Idea.create(idea_params)
+      render json: @idea
+      
+    end
+
+    def update
+
+      @idea = Idea.find(params[:id])
+      @idea.update_attributes(idea_params)
+      render json: @idea
+      
+    end
+
+    def destroy
+
+      @idea= Idea.find(params[:id])
+      
+      if @idea.destroy
+        head :no_content, status: :ok
+      else
+        render json: @ideas.errors, status: :unprocessable_entity
+      end
+      
+    end
+
+    private
+
+    def idea_params
+
+      params.require(:idea).permit(:title, :body)
       
     end
 
